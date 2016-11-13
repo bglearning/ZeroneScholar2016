55a5f80a505d3f3e44fb4e3deafe9603c0cba6ef
Bijay Gurung <bijay@fusemachines.com>
2016-11-13 12:51:32 +0545

    Add Script to build the output file
    Add Bash Script File that:
    
    - Compiles the tex file in each directory except `Output/`
    - Copies the output pdf files to `Output/`
    - Combines the pdf files into the single output file
    
    Uses:
    - pdflatex
    - pdfunite

--- build.sh
+++ build.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+output_folder="Output/"
+output_file="ZeroneScholar2016.pdf"
+# In each Directory
+for D in */;
+do
+    (
+    cd $D
+    # If the Directory is not Output
+    if [[ $D != $output_folder ]]
+    then
+        echo "Working on: ${D}"
+        # Compile each .tex file
+        for file in *.tex;
+        do
+            pdflatex $file
+            # Copy the output pdf to Output/ folder
+            cp "${file%.tex}.pdf" ../$output_folder
+        done
+    fi
+     )
+done
+
+cd $output_folder
+# Delete the previous version
+if [ -f $output_file ];
+then
+    rm $output_file
+fi
+
+# Unite the pdf into the Output File
+pdfunite $(ls -v *.pdf) $output_file
+
